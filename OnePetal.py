c9d096f43f1251b14024b0283d6a3e6b829ec210
fanwentao <fwt881881@hotmail.com>
2026-07-23 19:50:43 -0400

    Add OnePetal billiard

---
+++ OnePetal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+plt.rcParams["figure.dpi"] = 500
+plt.rcParams['axes.linewidth'] = 0.1
+class OnePetalObj(object):
+    def __init__(self, R, phistar):
+        self.R=R
+        self.phistar=phistar
+        self.Phistar=np.arcsin(math.sin(phistar) / R)
+    def plotphaseMr(self):
+        thetaline=np.linspace(0, self.phistar, 500)
+        phiconstant=np.linspace(self.phistar, 2*math.pi-self.phistar, 500)
+        plt.plot(phiconstant, thetaline,'k', lw=0.2)
+
